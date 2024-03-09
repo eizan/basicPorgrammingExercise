497a34228df41c2b082a6d75031aafca53935a67
ikhsant <39888845+ikhsant@users.noreply.github.com>
2024-03-09 15:21:14 +0700

    penambahan python biodata

---
+++ latihan3/biodata.py
@@ -0,0 +1,17 @@
+# membuat variabel biodata
+nama = "Ikhsan"
+semester = 2
+kelas = "TI23B"
+jurusan = "Teknik Informatika"
+quote = "ngoding is my live"
+married = False
+adalahPerempuan = False
+hobi = ["nonton", "main game","Tidur"]
+
+print("Nama saya: ",nama)
+print("Semester: ",semester)
+print("Kelas: ",kelas)
+print("Jurusan: ",jurusan)
+print("Qutote saya: ",quote)
+print("Sudah menikah: ",married)
+print("Hobi: ",hobi)
